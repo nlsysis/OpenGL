f7d3edc7764f4386c9d6e0657815b639ff6ac35c
nlsysis <nlsysis@126.com>
2020-06-08 14:14:35 +0900

    7-use vertex array object
    attention the module in GLFW_OPENGL_CORE_PROFILE or GLFW_OPENGL_COMPAT_PROFILE

--- OpenGLStu/OpenGLStu/src/Application.cpp
+++ OpenGLStu/OpenGLStu/src/Application.cpp
@@ -111,6 +111,9 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(640, 481, "Hello World", NULL, NULL);
 	if (!window)
@@ -121,6 +124,8 @@ int main(void)
 	
 
 	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error" << std::endl;
@@ -138,6 +143,10 @@ int main(void)
 	 0, 1, 2,
 	 2, 3, 0
 	};
+
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
@@ -165,16 +174,39 @@ int main(void)
 	unsigned shader = CreateShader(source.vertexSource, source.fragmentSource);
 	glUseProgram(shader);
 
+	///Uniform
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location,0.2f,0.3f,0.8f,1.0f));
+
+	///Unbind index buffer and fragment buffer
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+	float r = 0.0f;
+	float increment = 0.05f;
 	///loop until the use close the window
 	while (!glfwWindowShouldClose(window))
 	{
 		///Render here
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		
-		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
-		
+		GLCall(glUseProgram(shader));
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+		
+		if (r > 1.0f)
+			increment = -0.5f;
+		if (r < 0.0f)
+			increment = 0.5f;
+
+		r += increment;
 		///Swap front and back buffers
 		glfwSwapBuffers(window);
 
