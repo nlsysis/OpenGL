800e487be00216fe096da7df3a418d3d38c601a4
nlsysis <nlsysis@126.com>
2020-06-16 10:55:23 +0900

    15-mutiply object
    acturally give one obj different mvp

--- OpenGLStu/OpenGLStu/src/Application.cpp
+++ OpenGLStu/OpenGLStu/src/Application.cpp
@@ -50,10 +50,10 @@ int main(void)
 	
 	{
 		float positions[] = {
-		100.0f, 100.0f, 0.0f, 0.0f,
-		200.0f, 100.0f, 1.0f, 0.0f,
-		200.0f, 200.0f, 1.0f, 1.0f,
-		100.0f, 200.0f, 0.0f, 1.0f
+		-50.0f, -50.0f, 0.0f, 0.0f,
+		 50.0f, -50.0f, 1.0f, 0.0f,
+		 50.0f,  50.0f, 1.0f, 1.0f,
+		-50.0f,  50.0f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -80,6 +80,8 @@ int main(void)
 		IndexBuffer ib(indices, 6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+
 		///shader
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
@@ -90,7 +92,7 @@ int main(void)
 		Texture texture("res/texture/index.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
-		shader.SetUniformMat4f("u_MVP", proj);
+		
 		///Unbind index buffer and fragment buffer
 		va.UnBind();
 		shader.UnBind();
@@ -111,6 +113,11 @@ int main(void)
 		bool show_another_window = false;
 		ImVec4 clear_color = ImVec4(0.45f, 100.05f, 0.60f, 1.00f);
 
+		glm::vec3 translationA = glm::vec3(100.0f, 100.0f, 0.0f);
+		glm::vec3 translationB = glm::vec3(200.0f, 200.0f, 0.0f);
+
+		glm::mat4 model, mvp;
+
 		///loop until the use close the window
 		while (!glfwWindowShouldClose(window))
 		{
@@ -121,37 +128,32 @@ int main(void)
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
-			renderer.Draw(va, ib, shader);
-
-			if (r > 1.0f)
-				increment = -0.5f;
-			if (r < 0.0f)
-				increment = 0.5f;
-
-			r += increment;
+			
+			{
+				model = glm::translate(glm::mat4(1.0f), translationA);
+				mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+		
+			{
+				model = glm::translate(glm::mat4(1.0f), translationB);
+				mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-
 				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
 				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
 				ImGui::Checkbox("Another Window", &show_another_window);
 
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 500.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 500.0f);
 				ImGui::End();
 			}
 
